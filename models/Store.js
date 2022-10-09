27f3d8b3786167f34312247d76c5dbba7673befb
SafiaJotey <safiaahmed1786@gmail.com>
2022-10-09 14:24:02 +0600

    Store schema design

---
+++ models/Store.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema.Types;
+const storeSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'please provide a store name'],
+      lowercase: true,
+      enum: {
+        values: ['Dhaka', 'chattogram', 'Rajshahi'],
+        message: '{VALUE} is not valid name',
+      },
+    },
+    description: String,
+    status: {
+      type: String,
+      enum: ['active', 'inactive'],
+      default: 'active',
+    },
+    manager: {
+      name: String,
+      contactNumber: String,
+      id: {
+        type: ObjectId,
+        ref: 'User',
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Store = mongoose.model('Store', storeSchema);
+module.exports = Store;
